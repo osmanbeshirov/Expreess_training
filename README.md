f47e617d07ada1640627792108a3c3e4c96d3784
osmanbeshirov <osman.beshirov@gmail.com>
2022-06-03 11:57:55 +0400

    new read ready

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Expreess_training
 1. npm init -y
-2. npm install express nodemon mysql
+2. npm install express nodemon mysql cors
+
+(cors for mysql work correctly)
 
 3.   "scripts": {
     "test": "echo \"Error: no test specified\" && exit 1", 
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const res = require('express/lib/response');
 const mysql = require('mysql');
-
-
+const cors = require('cors')
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     port: 3306,
@@ -18,25 +17,26 @@ connection.connect((err)=> {
 })
 
 
-
-
 const app = express();
-
 app.use(express.json());
-
+app.use(cors())
 
 app.get('/', (req,res) => {
-    connection.query('SELECT * FROM users', (err, data)=> {
-
-        if(data){
-            return res.send(data)
-        }
-
-    })
-
+    res.send('Server is true !!!')
 })
 
 
+app.get('/users', (req,res) => {
+    connection.query('SELECT * FROM users', (err, data)=> {
+
+        if(!data){
+            return res.status(400).send();
+        }
+        res.status(200).send(data)
+    
+    })
+
+})
 
 
 app.listen(3000, () => {
